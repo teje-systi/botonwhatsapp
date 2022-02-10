d6c92681b34ff09d5a086ff021ca57f19dcfc504
Javier Tejerina P <javiertp@gmail.com>
2022-02-09 22:13:02 -0400

    Corrección mensaje Whatsapp con otro enlace a google y wa.me

--- js/index.js
+++ js/index.js
@@ -17,8 +17,8 @@ function showPosition(position) {
   //var geolocalizacion = "https://wa.me/56984431800?text=Emergencia,%20en%20https://www.openstreetmap.org/?mlat="+ position.coords.latitude +"&&mlon="+ position.coords.longitude + "#map=17/"+ position.coords.latitude +"/"+ position.coords.longitude;
   //var geolocalizacion = "https://wa.me/56984431800?text=Emergencia,%20en%20https://www.openstreetmap.org/#map=17/"+ position.coords.latitude +"/"+ position.coords.longitude;
   //var geolocalizacion = "https://wa.me/56984431800?text=Me%20comunico%20desde%20TuWeb,%20tengouna%20emergencia%20en%20coordenadas%20Latitude:%20" + position.coords.latitude + ",%20Longitude:%20" + position.coords.longitude;
-  //var geolocalizacion = "https://wa.me/56984431800?text=Me%20comunico%20desde%20TuWeb,%20tengouna%20emergencia%20en%20https://www.google.com/maps/@"+ position.coords.latitude +","+ position.coords.longitude + "z"
-  var geolocalizacion = "http://maps.googleapis.com/maps/api/staticmap?center=" + position.coords.latitude + "," + position.coords.longitude + "&zoom=13&size=300x300&sensor=false";
+  //var geolocalizacion = "https://wa.me/56984431800?text=Me%20comunico%20desde%20TuWeb,%20tengo%20una%20emergencia%20en%20https://www.google.com/maps/@"+ position.coords.latitude +","+ position.coords.longitude + "z"
+  var geolocalizacion = "https://wa.me/56984431800?text=Me%20comunico%20desde%20TuWeb,%20tengo%20una%20emergencia%20en%20http://maps.googleapis.com/maps/api/staticmap?center=" + position.coords.latitude + "," + position.coords.longitude + "&zoom=13&size=300x300&sensor=false";
   document.getElementById("miUbicacion").setAttribute("href", geolocalizacion);
     x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude + "<br>Url: " + geolocalizacion;
